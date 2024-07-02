ebbf5ac7321d6123547abfe68d910c2c794caf0c
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-07-02 21:28:16 +1200

    Added MenusController and OrdersController

---
+++ Bunshack_Restaurant.Server/Controllers/MenusController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bunshack_Restaurant.Server.Models;
+using Bunshack_Restaurant.Server.Repositories.Abstract;
+
+namespace Bunshack_Restaurant.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenusController : ControllerBase
+    {
+        private readonly IMenuRepository _menuRepository;
+        public MenusController(IMenuRepository menuRepository)
+        {
+            _menuRepository = menuRepository;
+        }
+
+        // GET: api/Menus
+        [HttpGet]
+        public ActionResult<List<Menu>> GetAllMenus()
+        {
+            return Ok(_menuRepository.GetAllMenus());
+        }
+
+        // GET: api/Menus/5
+        [HttpGet("{id}")]
+        public ActionResult<Menu> GetMenuById(Guid id)
+        {
+            try
+            {
+                var menu = _menuRepository.GetMenuById(id);
+                return Ok(menu);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT: api/Menus/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public ActionResult<Menu> UpdateMenu(Guid id, Menu menu)
+        {
+            try
+            {
+                var menuUpdate = _menuRepository.UpdateMenuById(id, menu);
+                return Ok(menuUpdate);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: api/Menus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public ActionResult<Menu> AddMenu(Menu menu)
+        {
+            return Ok(_menuRepository.AddMenu(menu));
+        }
+
+        // DELETE: api/Menus/5
+        [HttpDelete("{id}")]
+        public ActionResult<Menu> DeleteMenu(Guid id)
+        {
+            try
+            {
+                var menu = _menuRepository.DeleteMenuById(id);
+                return Ok(menu);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Bunshack_Restaurant.Server/Controllers/OrdersController.cs
@@ -0,0 +1,80 @@
+﻿using Bunshack_Restaurant.Server.Models;
+using Bunshack_Restaurant.Server.Repositories.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bunshack_Restaurant.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+        public OrdersController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public ActionResult<List<Order>> GetAllOrders()
+        {
+            return Ok(_orderRepository.GetAllOrders());
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrderById(Guid id)
+        {
+            try
+            {
+                var order = _orderRepository.GetOrderById(id);
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public ActionResult<Order> ModifyOrder(Guid id, Order order)
+        {
+            try
+            {
+                order.Id = id;
+                var orderUpdate = _orderRepository.ModifyOrder(order);
+                return Ok(orderUpdate);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: api/Menus
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public ActionResult<Order> PlaceOrder(Order order)
+        {
+            return Ok(_orderRepository.PlaceOrder(order));
+        }
+
+        // DELETE: api/Menus/5
+        [HttpDelete("{id}")]
+        public ActionResult<Order> CancelOrder(Guid id)
+        {
+            try
+            {
+                var order = _orderRepository.CancelOrder(id);
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
