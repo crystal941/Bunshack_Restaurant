23b2c26e306e189291a23445321e7e2607c75245
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-07-05 21:23:33 +1200

    Initial Migration and ChangeUserTable Migration

---
+++ Bunshack_Restaurant.Server/Data/Migrations/20240703092623_ChangeUserTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bunshack_Restaurant.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CustomUsername",
+                table: "AspNetUsers",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AspNetUsers",
+                newName: "CustomUsername");
+        }
+    }
+}
