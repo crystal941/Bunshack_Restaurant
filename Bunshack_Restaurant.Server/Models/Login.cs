0bac2c585d710c359653f4f0d394072fbdbc13e8
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-07-05 21:06:03 +1200

    Updated Login and User Models

--- Bunshack_Restaurant.Server/Models/Login.cs
+++ Bunshack_Restaurant.Server/Models/Login.cs
@@ -5,5 +5,6 @@
         public string? Username { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+        public bool Remember { get; set; } = false;
     }
 }
--- Bunshack_Restaurant.Server/Models/User.cs
+++ Bunshack_Restaurant.Server/Models/User.cs
@@ -7,7 +7,7 @@ namespace Bunshack_Restaurant.Server.Models
     public class User : IdentityUser
     {
         [MaxLength(50)]
-        public string? CustomUsername { get; set; }
+        public string? Name { get; set; }
         [Column(TypeName = "datetime")]
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         [Column(TypeName = "datetime")]
