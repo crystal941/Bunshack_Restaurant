222e4fd1be26299784b5bfdb253cceba4f3fc0c7
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-07-07 21:32:06 +1200

    Add LoginController using Authorization

---
+++ Bunshack_Restaurant.Server/Controllers/LoginController.cs
@@ -0,0 +1,134 @@
+﻿using Bunshack_Restaurant.Server.Data.Context;
+using Bunshack_Restaurant.Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Bunshack_Restaurant.Server.Controllers
+{
+    [Route("api/LoginController")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(SignInManager<User> signInManager, UserManager<User> userManager, ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser(User user)
+        {
+            IdentityResult result = new();
+            try
+            {
+                User user_ = new User()
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    UserName = user.UserName,
+                    Role = user.Role,
+                };
+
+                result = await _userManager.CreateAsync(user_, user.PasswordHash);
+
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Something went wrong, please try again. " + ex.Message);
+            }
+            return Ok(new { message = "Registered Successfully.", result = result });
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginUser(Login login)
+        {
+            try
+            {
+                User user_ = await _userManager.FindByEmailAsync(login.Email);
+
+                if (user_ != null)
+                {
+                    login.Username = user_.UserName;
+
+                    if (!user_.EmailConfirmed)
+                    {
+                        user_.EmailConfirmed = true;
+                    }
+
+                    var result = await _signInManager.PasswordSignInAsync(user_, login.Password, login.Remember, false);
+
+                    if (!result.Succeeded)
+                    {
+                        return Unauthorized(new { message = "Check your login credentials and try again" });
+                    }
+
+                    user_.LastLogin = DateTime.Now;
+                    var updateResult = await _userManager.UpdateAsync(user_);
+
+                    var isAdmin = user_.IsAdmin;
+
+                    return Ok(new {message = "Login Successful.", result, isAdmin});
+                }
+                else
+                {
+                    return BadRequest(new { message = "Please check your credentials and try again. " });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Something went wrong, please try again. " + ex.Message });
+            }
+        }
+
+        [HttpGet("logout"), Authorize]
+        public async Task<ActionResult> LogoutUser()
+        {
+            try
+            {
+                await _signInManager.SignOutAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Something went wrong, please try again. " + ex.Message });
+            }
+
+            return Ok(new { message = "You are free to go!" });
+        }
+
+        [HttpGet("check")]
+        public async Task<ActionResult> CheckUserStatus()
+        {
+            try
+            {
+                var user = HttpContext.User;
+                var principals = new ClaimsPrincipal(user);
+                if (_signInManager.IsSignedIn(principals))
+                {
+                    var currentUser = await _signInManager.UserManager.GetUserAsync(principals);
+                    var isAdmin = currentUser.IsAdmin;
+                    return Ok(new { message = "Logged in", user = new { currentUser.Email, currentUser.UserName, isAdmin } }); 
+                }
+                else
+                {
+                    return Forbid();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Something went wrong, please try again.", error = ex.Message });
+            }
+        }
+    }
+}
