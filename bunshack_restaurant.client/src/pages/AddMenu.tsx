6490f70d5e79ee946055f16baca03740e5f8c53b
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-07-11 12:00:22 +1200

    Created a Page for admin to add new menu

---
+++ bunshack_restaurant.client/src/pages/AddMenu.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import {
+    Box,
+    Button,
+    Container,
+    Grid,
+    TextField,
+    Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Layout from "../components/Layout";
+import { Link } from 'react-router-dom';
+
+const AddMenuPage: React.FC = () => {
+    const navigate = useNavigate();
+    const [foodName, setFoodName] = useState("");
+    const [price, setPrice] = useState("");
+
+    const addMenuHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const newMenu = {
+            FoodName: foodName,
+            Price: parseFloat(price),
+        };
+
+        try {
+            const response = await fetch("/api/MenusController", {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify(newMenu),
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+            });
+
+            //const data = await response.json();
+
+            if (response.ok) {
+                navigate("/admin"); // Redirect to all menus list page
+            }
+        } catch (error) {
+            console.error("Add menu error:", error);
+        }
+    };
+
+    return (
+        <Layout>
+            <div className="content">
+                <Container sx={{ mb: 15, "@media(max-width: 600px)": { mb: 10 } }}>
+
+                    <Typography variant="h4" style={{ color: 'black' }} >
+                        Add New Menu Item
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            maxWidth: "400px",
+                            margin: "auto",
+                            padding: "20px",
+                            border: "1px solid #ccc",
+                            borderRadius: "5px",
+                            backgroundColor: "#fff",
+                            "@media(max-width: 600px)": { "& h4 ": { fontSize: "1.5rem" } },
+                        }}
+                    >
+                        <form className="add-menu" onSubmit={addMenuHandler}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="foodName"
+                                        label="Food Name"
+                                        value={foodName}
+                                        onChange={(e) => setFoodName(e.target.value)}
+                                        required
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        fullWidth
+                                        id="price"
+                                        label="Price"
+                                        type="number"
+                                        value={price}
+                                        onChange={(e) => setPrice(e.target.value)}
+                                        required
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                sx={{ mt: "20px" }}
+                                type="submit"
+                                variant="contained"
+                                color="success"
+                                fullWidth
+                            >
+                                Add Menu
+                            </Button>
+                            <Button variant="contained" color="primary" sx={{ mt: "20px" }} fullWidth component={Link}
+                                to={"/admin"}>Back to All Menus</Button>
+                        </form>
+                    </Box>
+                </Container>
+            </div>
+        </Layout>
+    );
+};
+
+export default AddMenuPage;
